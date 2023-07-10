96f9cb7c67fd28d0f9e6d805173af42f5c59a1bb
Carlos Mencias <menciasleal@gmail.com>
2023-07-09 23:41:02 -0500

    Agregar archivos de proyecto.

---
+++ Compra/Producto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Producto
+{
+    public string? nombre;
+    public decimal precio;
+    public int cantidadDisponible;
+
+    public Producto()
+    {
+        nombre = null;
+        precio = 0;
+        cantidadDisponible = 0;
+    }
+
+    public Producto(string nombre, decimal precio, int cantidadDisponible)
+    {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public decimal CalcularCostoTotal()
+    {
+        return precio * cantidadDisponible;
+    }
+
+
+    public void ActualizarCantidad(int cantidadComprada)
+    {
+        cantidadDisponible -= cantidadComprada;
+    }
+
+    public void MostrarDetalles()
+    {
+        Console.WriteLine($"Nombre................:{nombre,20}\n");
+        Console.WriteLine($"Precio................:{precio,20:C2}\n");
+        Console.WriteLine($"Cantidad disponible...:{cantidadDisponible,20}\n");
+    }
+}
---
+++ Compra/Program.cs
@@ -0,0 +1,28 @@
+﻿
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Producto producto = new Producto
+
+        {
+            nombre = "Peras",
+            precio = 0.50m,
+            cantidadDisponible = 50
+        };
+
+
+        producto.MostrarDetalles();
+
+        Console.Write("Ingrese la cantidad deseada: ");
+        int cantidadCompra = Convert.ToInt32(Console.ReadLine());
+        producto.ActualizarCantidad(cantidadCompra);
+
+        Console.WriteLine("\nDespués de la compra:");
+        producto.MostrarDetalles();
+
+        decimal costoTotal = producto.CalcularCostoTotal();
+        Console.WriteLine("\nCosto total de los productos disponibles: $" + costoTotal);
+    }
+}
